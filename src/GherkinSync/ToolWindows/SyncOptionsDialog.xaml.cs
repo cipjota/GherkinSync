1a8a1e1d077eed84bd5075b3a4a5f148d57bbb00
cipjota <cipriano@cjweb.pt>
2025-04-29 17:26:28 +0100

    Get test plan and test suite info

---
+++ src/GherkinSync/ToolWindows/SyncOptionsDialog.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.PlatformUI;
+using System.Windows;
+
+namespace GherkinSync.ToolWindows
+{
+    public partial class SyncOptionsDialog : DialogWindow
+    {
+        public SyncOptionsDialog()
+        {
+            InitializeComponent();
+
+            // Work around for bug https://github.com/microsoft/XamlBehaviorsWpf/issues/86
+            var _ = new Microsoft.Xaml.Behaviors.DefaultTriggerAttribute(typeof(Trigger), typeof(Microsoft.Xaml.Behaviors.TriggerBase), null);
+        }
+
+        public SyncOptionsDialogViewModel SyncOptions { get; private set; } = new SyncOptionsDialogViewModel();
+    }
+}
